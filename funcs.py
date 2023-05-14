7d527d582bcac3e06ddb7ab78cc133e9bc2f8106
igorK <i.kolegov78@gmail.com>
2023-05-14 20:37:16 +0300

    add file

---
+++ funcs.py
@@ -0,0 +1,12 @@
+def plus():
+    num1 = int(input())
+    num2 = int(input())
+    result = num1 + num2
+    return result
+
+def minus():
+    num1 = int(input())
+    num2 = int(input())
+    result = num1 - num2
+    return result
+
